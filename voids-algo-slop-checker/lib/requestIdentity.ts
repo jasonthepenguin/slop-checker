0d71dab8123786b6c018c4ba3cfb02b998c148e5
Jason Botterill <jasonbotterill@hotmail.com>
2025-09-18 20:49:25 +0800

    add type fix nextrequest not having IP property

--- voids-algo-slop-checker/lib/requestIdentity.ts
+++ voids-algo-slop-checker/lib/requestIdentity.ts
@@ -5,14 +5,14 @@ const FALLBACK_IDENTIFIER = 'unknown';
 const stripIpv6Prefix = (value: string) => value.replace(/^::ffff:/, '');
 
 export function getClientIdentifier(request: NextRequest) {
-  const directIp = request.ip;
-  if (directIp) {
-    return stripIpv6Prefix(directIp);
-  }
-
   const realIp = request.headers.get('x-real-ip');
   if (realIp) {
     return stripIpv6Prefix(realIp);
+  }
+
+  const vercelIp = request.headers.get('x-vercel-ip');
+  if (vercelIp) {
+    return stripIpv6Prefix(vercelIp);
   }
 
   const forwardedFor = request.headers.get('x-forwarded-for');
